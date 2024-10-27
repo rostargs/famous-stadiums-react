3c8528e63f86ea60e4b3e0d4894d473c77559735
Rostyslav <rostiksavelko@gmail.com>
2024-10-27 18:14:54 +0200

    fixed bug

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,24 +1,21 @@
 import "./Navigation.scss";
 import { navigation } from "../../data";
-import Portal from "../Portal/Portal";
 import Container from "../Container/Container";
 import NavItem from "../NavList/NavItem";
 
 const Navigation = ({ isVisible, setMenu }) => {
     return (
-        <Portal id="nav-portal">
-            <div className={`navigation ${isVisible ? "navigation--visible" : ""}`}>
-                <Container>
-                    <div className="navigation__content">
-                        <ul>
-                            {navigation.map((item, index) => (
-                                <NavItem {...item} key={index} onClick={() => setMenu(false)} />
-                            ))}
-                        </ul>
-                    </div>
-                </Container>
-            </div>
-        </Portal>
+        <div className={`navigation ${isVisible ? "navigation--visible" : ""}`}>
+            <Container>
+                <div className="navigation__content">
+                    <ul>
+                        {navigation.map((item, index) => (
+                            <NavItem {...item} key={index} onClick={() => setMenu(false)} />
+                        ))}
+                    </ul>
+                </div>
+            </Container>
+        </div>
     );
 };
 
